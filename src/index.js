6c6c5b7589747a931c0f5b491f2906d1c107604f
Alena Ahafonova <cool-5@tut.by>
2021-02-22 14:43:50 +0300

    refactor:add code reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function reverse (n) {
-  
-}
+    if (n>=0){
+    return (
+        parseFloat(
+          n
+            .toString()
+            .split('')
+            .reverse()
+            .join('')
+        ) * Math.sign(n)
+      )   }
+      else
+      { 
+        return  +("" + -n).split("").reverse().join("");
+      
+    }   
+}           
+
